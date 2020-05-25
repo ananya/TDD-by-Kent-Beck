b25cfba57b828ebe43e4fb5da360eab2cdf4975f
ananya <ananyaagrawal125@gmail.com>
2020-05-25 19:25:39 +0530

    removing some dup test

--- test_currencies.py
+++ test_currencies.py
@@ -11,14 +11,7 @@ class CurrencyTest(TestCase):
     def testEquality(self):
         self.assertEqual(Money.dollar(5), Money.dollar(5))  
         self.assertNotEqual(Money.dollar(5), Money.dollar(6))   
-        self.assertEqual(Money.franc(5), Money.franc(5))  
-        self.assertNotEqual(Money.franc(5), Money.franc(6))   
         self.assertNotEqual(Money.franc(5), Money.dollar(5))   
-
-    def testFrancMultiplication(self):
-        five = Money.franc(5)
-        self.assertEqual(Money.franc(10),five.times(2))
-        self.assertEqual(Money.franc(15),five.times(3))
 
     def testCurrency(self):
         self.assertEqual("USD", Money.dollar(1).currency)
