c8ff9c0af8e8dcf7b118bebb135f76e13b9e7df3
ananya <ananyaagrawal125@gmail.com>
2020-05-25 16:21:37 +0530

    test 3 remove duplicate for amount and equal

--- finance.py
+++ finance.py
@@ -3,22 +3,17 @@
 # static method called on class
 # normal methods called on object(e.g times)
 
-class Dollar():
+class Money():
     def __init__(self, amount):
         self.amount = amount
     
     def __eq__(self, other):
         return self.amount == other.amount
 
+class Dollar (Money):
     def times(self, multiplier):
         return Dollar(self.amount*multiplier)
 
-class Franc():
-    def __init__(self, amount):
-        self.amount = amount
-    
-    def __eq__(self, other):
-        return self.amount == other.amount
-
+class Franc(Money):
     def times(self, multiplier):
         return Franc(self.amount*multiplier)
